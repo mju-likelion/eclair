db55fb0b47f435d9289fcac8e245c5117e49d669
zelkovaria <hyein396@gmail.com>
2024-02-27 19:44:03 +0900

    feat: 로그인 메인 기본 구성 구현 - #5

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,5 +1,38 @@
+import { Axios } from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const LogIn = () => {
-  return <>로그인</>;
+  const navigate = useNavigate();
+  const [managerId, setManagerId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleFormSubmit = async (event) => {
+    console.log({ managerId, password });
+    try {
+      const response = await Axios.post('/auth/login', {
+        managerId: managerId,
+        password: password,
+      });
+      console.log(event);
+      const statusCode = response.data.statusCode;
+      if (statusCode === '200') {
+        navigate('/main');
+        console.log('/main');
+      }
+    } catch (error) {
+      alert('오류발생');
+    }
+  };
+
+  return (
+    <form onSubmit={handleFormSubmit}>
+      로그인
+      <input value={managerId} onChange={(e) => setManagerId(e.target.value)} />
+      <input value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">로그인</button>
+    </form>
+  );
 };
 
 export default LogIn;
