b91876df28bedacc7a0491e29f3973d17765153f
zelkovaria <hyein396@gmail.com>
2024-02-28 00:49:09 +0900

    feat: 로그아웃 기능 구현 - #5

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,31 @@
+import styled from 'styled-components';
+import { Axios } from '../api/Axios';
+import { useNavigate } from 'react-router-dom';
+
 const Main = () => {
-  return <>메인 페이지</>;
+  const navigate = useNavigate();
+  const onClick = async () => {
+    try {
+      const response = await Axios.get('/auth/logout');
+      const statusCode = response.data.statusCode;
+
+      if (statusCode === '200') {
+        alert('로그아웃 됐습니다');
+        navigate('/login');
+      }
+    } catch {
+      alert('로그아웃 실패');
+    }
+  };
+  return (
+    <>
+      <LogoutButton onClick={onClick}>로그아웃</LogoutButton>
+    </>
+  );
 };
+
+const LogoutButton = styled.button`
+  border-radius: 8px;
+`;
 
 export default Main;
