2133f1bf0febab3c6873e70b719730b494f7ae6c
hyunyeee <5004ohy@naver.com>
2024-02-27 13:47:52 +0900

    setting: 라우터 세팅 - #1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import LogIn from './pages/LogIn';
+import ApplicationView from './pages/ApplicationView';
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/:studentId" element={<ApplicationView />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
