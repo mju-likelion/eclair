6dca291e038857218dde505ecc87cf3675b2061c
hyunyeee <5004ohy@naver.com>
2024-03-04 03:36:39 +0900

    feat: 로그인 상태에 따른 페이지 redirect - #23

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
 import LogIn from './pages/LogIn';
+import Application from './pages/Application';
 import Introduction from './pages/Introduction';
-import { useState } from 'react';
 
 function App() {
   const [isLoggedin, setIsLoggedin] = useState(false);
   return (
     <BrowserRouter>
       <Routes>
+        <Route index path="/" element={<Main />} />
         <Route path="/introduces" element={<Introduction />} />
         <Route
-          path="/"
+          path="/applications"
           element={
-            <Main isLoggedin={isLoggedin} setIsLoggedin={setIsLoggedin} />
+            <Application
+              isLoggedin={isLoggedin}
+              setIsLoggedin={setIsLoggedin}
+            />
           }
         />
         <Route
