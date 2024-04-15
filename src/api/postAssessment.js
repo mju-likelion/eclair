8f11ff9e4bbfeca83408f01bc4c44a5148445d00
zelkovaria <hyein396@gmail.com>
2024-04-15 22:17:04 +0900

    fix: 오타 수정

--- src/api/postAssessment.js
+++ src/api/postAssessment.js
@@ -3,7 +3,7 @@ import { Axios } from './Axios';
 export const postAssessment = async (result, id) => {
   try {
     const path = result === 'approve' ? 'approve' : 'reject';
-    const url = `/application/${path}`;
+    const url = `/applications/${path}`;
 
     const response = await Axios.patch(url, {
       applicationId: id,
